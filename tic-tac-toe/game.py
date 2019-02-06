14c8f797297783be794016d565972ed333c08f78
renph <renph@qq.com>
2019-02-06 17:24:16 +0000

    add model

--- tic-tac-toe/game.py
+++ tic-tac-toe/game.py
@@ -1,12 +1,57 @@
 from PyQt5 import QtGui
-from PyQt5.QtCore import QThread, Qt
+from PyQt5.QtCore import QThread, Qt, QPoint
 from PyQt5.QtGui import QPalette, QColor, QPainter
 from PyQt5.QtWidgets import QMainWindow, QWidget, QApplication
 
 
 class TicTacToeModel:
     def __init__(self):
-        self.board = [0 for _ in range(9)]
+        self.board = [[0 for _ in range(3)] for _ in range(3)]
+        self.nextMove = 1
+        self.lastMove = (0, 0, 0)
+        self.nMove = 0
+
+    def __getitem__(self, item):
+        return self.board[item]
+
+    def get(self, row, col):
+        if 0 <= row <= 2 and 0 <= col <= 2:
+            return self.board[row][col]
+
+    def move(self, row, col):
+        # print('move to',row,col)
+        self.board[row][col] = self.nextMove
+        self.lastMove = row, col, self.nextMove
+        self.nextMove = 0 - self.nextMove
+        self.nMove += 1
+
+    def isGameOver(self):
+        r, c, p = self.lastMove
+        if p == 0:
+            return False
+        if self.nMove > 8:
+            return True
+        # count number of consecutive chess in 4 directions
+        dirCnt = [1 for _ in range(4)]
+        consecutive = [[True, True] for _ in range(4)]
+        for i in range(1, 3):  # i from 1 to 2
+            dirPos = [(r - i, c, r + i, c), (r, c - i, r, c + i),
+                      (r - i, c - i, r + i, c + i), (r - i, c + i, r + i, c - i)]
+            for d in range(4):  # check 4 directions
+                r1, c1, r2, c2 = dirPos[d]
+                if consecutive[d][0]:
+                    if self.get(r1, c1) == p:
+                        dirCnt[d] += 1
+                    else:
+                        consecutive[d][0] = False
+                if consecutive[d][1]:
+                    if self.get(r2, c2) == p:
+                        dirCnt[d] += 1
+                    else:
+                        consecutive[d][1] = False
+                if dirCnt[d] >= 3:
+                    return True
+        return False
 
 
 class TicTacToeController:
@@ -22,6 +67,15 @@ class TicTacToeController:
         self.isNewGame = True
         self.view.update()
 
+    def move(self, row, col):
+        if self.isNewGame:
+            if 0 <= row <= 2 and 0 <= col <= 2 and self.model[row][col] == 0:
+                self.model.move(row, col)
+                self.view.update()
+                if self.model.isGameOver():
+                    self.isNewGame = False
+                    print("Game over")
+
 
 class TicTacToeView(QMainWindow):
     def __init__(self, controller):
@@ -36,7 +90,7 @@ class TicTacToeView(QMainWindow):
         self.setMenuWidget(meanBar)
 
         # self.resize(QSize(640, 640))
-        self.setFixedSize(640, 640 + 30+ 30)
+        self.setFixedSize(640, 640 + 30 + 30)
         # self.setWindowFlags(Qt.WindowMinimizeButtonHint | Qt.WindowCloseButtonHint)
         self.setAutoFillBackground(True)
         palette = QPalette()
@@ -72,6 +126,32 @@ class TicTacToeView(QMainWindow):
                              board_y + board_height)
             painter.drawLine(board_x, board_div * (i + 1) + board_y, board_x + board_width,
                              board_div * (i + 1) + board_y)
+
+        for i in range(3):
+            for j in range(3):
+                p = self.controller.model.get(j, i)
+                if p == 0:
+                    continue
+                if p == 1:
+                    pen.setColor(QColor("#db0000"))
+                    painter.setPen(pen)
+                    painter.drawLine(board_x + board_div * i + 50, board_y + board_div * j + 50,
+                                     board_x + board_div * i + 150, board_y + board_div * j + 150)
+                    painter.drawLine(board_x + board_div * i + 50, board_y + board_div * j + 150,
+                                     board_x + board_div * i + 150, board_y + board_div * j + 50)
+                elif p == -1:
+                    pen.setColor(QColor("#00468b"))
+                    painter.setPen(pen)
+                    painter.drawEllipse(QPoint(board_x + board_div * i + 100,
+                                               board_y + board_div * j + 100),
+                                        60, 60)
+
+    def mouseReleaseEvent(self, a0: QtGui.QMouseEvent):
+        board_x, board_y, board_width, board_height = self.boardSize
+        my = (a0.x() - board_x) // 200
+        mx = (a0.y() - board_y) // 200
+        self.controller.move(mx, my)
+
 
 if __name__ == '__main__':
     app = QApplication([])
